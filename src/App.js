8796749ba9dd49c3a9e63d8b64e5b42d7966b83d
SafeCo <77068178+SafeCo@users.noreply.github.com>
2022-10-25 03:38:53 +0100

    merging route to main

--- src/App.js
+++ src/App.js
@@ -1,34 +1,3 @@
-<<<<<<< HEAD
-import React, {useEffect, useRef, useState} from 'react'; 
-import './App.css';
-import './ImageUpload.css'
-import Post from './Post';
-import Reel from './Reel';
-import FriendSuggestion from './FriendSuggestion';
-import { db, auth, storage} from './firebase';
-import Box from '@mui/material/Box';
-import Modal from '@mui/material/Modal';
-import Button from '@mui/material/Button';
-import { Input } from '@mui/material';
-import ImageUpload from './ImageUpload';
-import searchIcon from './search.svg';
-import InstaLogo from './instagram-text-icon.svg'
-
-
-const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: 400,
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
-  boxShadow: 24,
-  pt: 2,
-  px: 4,
-  pb: 3,
-};
-=======
 import React from 'react'
 import './App.css'
 import { Routes, Route } from "react-router-dom";
@@ -36,7 +5,6 @@ import LoginPage from './LoginPage'
 import HomePage from './HomePage'
 import { ProtectedRoute } from "./ProtectedRoute";
 import {AuthProvider} from "./hooks/useAuth"
->>>>>>> Routes
 
 function App() {
   return (
