7d1df5a95f126c4b08ff7adcafd1888701a165a2
SafeCo <77068178+SafeCo@users.noreply.github.com>
2022-10-26 15:02:14 +0100

    Update to SearchBar.js

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,13 +1,27 @@
-import React from 'react'
+import React,{useState} from 'react'
 import './SearchBar.css'
 import searchIcon from './search.svg';
 
 
 function SearchBar() {
+    const [searchText, setSearchText] = useState('')
+
+    const [focused, setFocused] = React.useState(false)
+    const onFocus = () => setFocused(true)
+    const onBlur = () => setFocused(false)
+    const itemsStyle = focused ? {display: 'none'} : {display: 'flex'}
+
     return (
         <div className="sB__searchBar">
-        <input type="text" className="sB__Input" />
-            <div className="sB__iconItems">
+            <input 
+            onFocus={onFocus} 
+            onBlur={onBlur}
+            type="text" 
+            className="sB__Input" 
+            value={searchText}
+            onChange={(e)=> setSearchText(e.target.value)}
+            />
+            <div style={itemsStyle} className="sB__iconItems">
                 <img className="sB__searchIcon" src={searchIcon} alt='Search Icon'/>
                 <span>Search</span>
             </div>
