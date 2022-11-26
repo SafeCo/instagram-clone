0beb7fa7a163fcbd39700c92952806c57355a0cb
SafeCo <77068178+SafeCo@users.noreply.github.com>
2022-11-26 14:13:33 +0000

    Update to Home page
    added cleanup to post useeffect

--- src/homePage/HomePage.js
+++ src/homePage/HomePage.js
@@ -1,52 +1,33 @@
-import React, {useEffect, useRef, useState, lazy, Suspense } from 'react'; 
+import React, {useEffect, useState, lazy, Suspense } from 'react'; 
 import { useOutletContext } from 'react-router-dom'
+import { db } from '../firebase';
 
 import './HomePage.css';
+
 import FriendSuggestion from './components/suggestions/FriendSuggestion';
-import { db, auth } from '../firebase';
 import ReelCarousel from './components/reelCarousel/ReelCarousel';
 import LazyPost from './components/lazyComponents/LazyPost';
-
 
 const Post = lazy(() => import('./components/posts/post/Post'))
 
 
 function HomePage() {
-	
-	// NORMALLY BELOW CONTEXT BUT ADDED USEFFECT TO CALL DIRECTLY FROM FIREBASE
-	//THERE IS AN ISSUE WHERE NEW ACCOUNT ARE NOT GETTING THE DISPLAYNAME 
-	//MOST LIKELY DUE TO THE PAGE CHANGING BEFORE FIREBASE CAN BE UPDATED
-
-	// const { user } = useContext(AuthContext);
-	
 	const [posts, setPosts] = useState([]);
-
-	const shouldLogOne = useRef(true)
-	const shouldLogTwo = useRef(true)
-	
-	//  None of the Firebase Authentication APIs cost money, except for phone authentication after the free monthly quota has been exhausted
-
 	const {user}= useOutletContext()
 	const {userInfo} = useOutletContext()
 
-
-//Putting posts on page
+	//Placing posts on page
 	useEffect(()=>{
-		if(shouldLogTwo.current){
-				shouldLogTwo.current = false;
-				db.collection('posts').orderBy('timestamp', 'desc').onSnapshot(snapshot => {
-				setPosts(snapshot.docs.map(doc => ({
-				id: doc.id,
-				post: doc.data(),
-				})));
-		})}
+			const unsubscribe = db.collection('posts').orderBy('timestamp', 'desc').onSnapshot(snapshot => {
+				setPosts(snapshot.docs.map(doc => (
+					{
+						id: doc.id,
+						post: doc.data(),
+					}
+				)));
+			})
+			return ()=> unsubscribe();        
 		}, []);
-
-
-	
-	// Each element like insta logo, search bar and icons have their container within the main contianer
-	// flex grow and shrink is used on the individual containers rather than the element
-	// the element is merely moved around with justify content flex start and end
 
 	return (
 		<>
@@ -65,20 +46,13 @@ function HomePage() {
 									}
 							</div>
 					</div>
-					
-
-
 					<div className="app__friendSugg">
 						<FriendSuggestion profileUsername={userInfo.username} userPhotoUrl={userInfo.photoUrl}/>
 					</div> 
 				</section>
-				
-		
 			</main>
-			
     	</>
 	)
-
 }
 
 export default HomePage
